4a64eaad962ded31418d302132c4604ac14e8a6b
ChangPyeong Jeon <connor.tavi@gmail.com>
2024-04-30 23:40:42 +0900

    [Init] _ YOM git init

---
+++ YOMMA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YOMMA
+//
+//  Created by 전창평 on 4/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
