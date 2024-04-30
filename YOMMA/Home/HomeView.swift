32155bcabce8f48ffa837cc5a6e8d90fef5858b5
ChangPyeong Jeon <connor.tavi@gmail.com>
2024-05-01 00:33:25 +0900

    [Feat] _ CodeBase Setting

---
+++ YOMMA/Home/HomeView.swift
@@ -0,0 +1,17 @@
+//
+//  HomeView.swift
+//  YOMMA
+//
+//  Created by 전창평 on 5/1/24.
+//
+
+import Foundation
+import UIKit
+
+class HomeView: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("========== test HomeView ===========")
+    }
+}
---
+++ YOMMA/Splash/SplashView.swift
@@ -0,0 +1,22 @@
+//
+//  SplashView.swift
+//  YOMMA
+//
+//  Created by 전창평 on 5/1/24.
+//
+
+import Foundation
+import UIKit
+import Then
+import SnapKit
+
+class SplashView: UIViewController {
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        splashSet()
+    }
+    
+    private func splashSet() {
+    }
+}
